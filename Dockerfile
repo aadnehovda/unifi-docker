4686a6582f72700fa5caf5c9e984a6164b04938c
Ådne Hovda <ahovda@openit.com>
2020-03-30 21:35:37 +0200

    updated version

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM centos:8 AS downloader
 RUN yum install -y unzip
 
-ARG version 5.12.22
+ARG version=5.12.66
 
 ADD https://www.ubnt.com/downloads/unifi/$version/UniFi.unix.zip .
 RUN unzip UniFi.unix.zip
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 mkdir -p data
 
-echo "unifi.http.port=80" >> data/system.properties
-echo "unifi.https.port=443" >> data/system.properties
+echo "unifi.http.port=${HTTP_PORT:-8080}" >> data/system.properties
+echo "unifi.https.port=${HTTPS_PORT:-8443}" >> data/system.properties
+echo "unifi.stun.port=${STUN_PORT:-3478}" >> data/system.properties
 echo "unifi.logStdout=true" >> data/system.properties
 exec java -jar lib/ace.jar $@
