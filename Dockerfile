3f3b3f745b16a8dc5af3996ea953c00609bed763
Aadne Hovda <aadne@hovda.no>
2021-11-22 22:57:59 +0100

    changed to crudinit, updated version

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM centos:8 AS downloader
 RUN yum install -y unzip
 
-ARG version=6.0.36
+ARG version=6.5.53
 
 ADD https://www.ubnt.com/downloads/unifi/$version/UniFi.unix.zip .
 RUN unzip UniFi.unix.zip
 
 FROM centos:8
 ADD mongodb.repo /etc/yum.repos.d/
-RUN yum install -y java-1.8.0-openjdk-headless mongodb-org-server && yum clean -y all && rm -fr /var/cache/yum
+RUN yum install -y epel-release && yum install -y crudini java-1.8.0-openjdk-headless mongodb-org-server && yum clean -y all && rm -fr /var/cache/yum
 WORKDIR /UniFi
 COPY --from=downloader /UniFi/ ./
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 mkdir -p data
 
-echo "unifi.http.port=${HTTP_PORT:-8080}" >> data/system.properties
-echo "unifi.https.port=${HTTPS_PORT:-8443}" >> data/system.properties
-echo "unifi.stun.port=${STUN_PORT:-3478}" >> data/system.properties
-echo "unifi.logStdout=true" >> data/system.properties
+crudini --set data/system.properties '' unifi.http.port ${HTTP_PORT:-8080}
+crudini --set data/system.properties '' unifi.https.port ${HTTPS_PORT:-8443}
+crudini --set data/system.properties '' unifi.stun.port ${STUN_PORT:-3478}
+crudini --set data/system.properties '' unifi.logStdout true
+
 exec java -jar lib/ace.jar $@
